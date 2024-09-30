24f603be57cf9983334fbf254175ecbcd4756777
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2024-09-30 15:23:06 +0200

    Update DB version

--- recon/core/base.py
+++ recon/core/base.py
@@ -246,7 +246,7 @@ class Recon(framework.Framework):
         self.query('CREATE TABLE IF NOT EXISTS profiles (username TEXT, resource TEXT, url TEXT, category TEXT, contact_id INTEGER, notes TEXT, module TEXT)')
         self.query('CREATE TABLE IF NOT EXISTS repositories (name TEXT, owner TEXT, description TEXT, resource TEXT, category TEXT, url TEXT, notes TEXT, module TEXT)')
         self.query('CREATE TABLE IF NOT EXISTS dashboard (module TEXT PRIMARY KEY, runs INT)')
-        self.query('PRAGMA user_version = 10')
+        self.query('PRAGMA user_version = 11')
 
     def _migrate_db(self):
         db_version = lambda self: self.query('PRAGMA user_version')[0][0]
